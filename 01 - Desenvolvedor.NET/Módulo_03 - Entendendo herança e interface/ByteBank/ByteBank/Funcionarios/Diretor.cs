de88b04feb86e44372c3dd48d606ddbd0432f8f2
Sara Gabriely SILVAsS <sara.silva10@fatec.sp.gov.sbr>
2019-01-25 12:04:55 -0200

    24.01.19 - 3

--- 01 - Desenvolvedor.NET/Módulo_03 - Entendendo herança e interface/ByteBank/ByteBank/Funcionarios/Diretor.cs
+++ 01 - Desenvolvedor.NET/Módulo_03 - Entendendo herança e interface/ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -10,7 +10,7 @@ namespace ByteBank.Funcionarios
     {
         // Ao chamar o construtor de um tipo derivado, será chamado primeiro
         // o construtor da classe base (no caso Funcionario)
-        public Diretor(double salario, string cpf) : base(salario, cpf)
+        public Diretor(string cpf) : base(5000, cpf)
         {
             Console.WriteLine("Criando DIRETOR");
             Console.WriteLine();
@@ -20,7 +20,10 @@ namespace ByteBank.Funcionarios
         public override double GetBonificacao()
         {
             //  return Salario + ( Salario * 0.10 );
-            return Salario + base.GetBonificacao();
+            // return Salario + base.GetBonificacao();
+
+            return Salario * 0.5;
+
             // está somando a bonificação do diretor (salario) com 
             // a bonificação de funcionario implementada na classe original (salario * 0.10)
 
--- 01 - Desenvolvedor.NET/Módulo_03 - Entendendo herança e interface/ByteBank/ByteBank/Program.cs
+++ 01 - Desenvolvedor.NET/Módulo_03 - Entendendo herança e interface/ByteBank/ByteBank/Program.cs
@@ -11,6 +11,11 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            CalcularBonificacao();
+
+
+
+            /* Primeiro código
             GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
 
             // Carlos
@@ -59,9 +64,38 @@ namespace ByteBank
             Console.WriteLine();
 
             Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
-            Console.ReadLine();
+            */
 
             Console.ReadLine();
         }
+
+        public static void CalcularBonificacao()
+        {
+            GerenciadorBonificacao gerenciadorBonificacao = new GerenciadorBonificacao();
+
+            Designer pedro = new Designer("833.222.048-39");
+            pedro.Nome = "Pedro";
+
+            Diretor roberta = new Diretor("159.753.398-04");
+            roberta.Nome = "Roberta";
+
+            Auxiliar igor = new Auxiliar("981.198.778-53");
+            igor.Nome = "Igor";
+
+            GerenteDeConta camila = new GerenteDeConta("326.982.628-89");
+            camila.Nome = "Camila";
+
+            gerenciadorBonificacao.Registrar(pedro);
+            gerenciadorBonificacao.Registrar(roberta);
+            gerenciadorBonificacao.Registrar(igor);
+            gerenciadorBonificacao.Registrar(camila);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Total de bonificações do mês: - R$ " +
+                gerenciadorBonificacao.GetTotalBonificacao());
+
+        }
+
     }
 }
