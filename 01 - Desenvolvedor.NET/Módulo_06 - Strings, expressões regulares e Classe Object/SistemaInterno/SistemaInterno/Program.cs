c81f8259b6f8d325d4f84a688eab906bf48fb232
Sara Gabriely SILVAsS <sara.silva10@fatec.sp.gov.sbr>
2019-02-03 12:57:36 -0200

    03.02.19 - 01

---
+++ 01 - Desenvolvedor.NET/Módulo_06 - Strings, expressões regulares e Classe Object/SistemaInterno/SistemaInterno/Program.cs
@@ -0,0 +1,67 @@
+﻿using ByteBank;
+using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateFimPagamento  = new DateTime(2019, 08, 17);
+
+            DateTime dataCorrente      = DateTime.Now;
+
+            // TimeSpan - Diferença entre datas
+            TimeSpan diferenca = TimeSpan.FromMinutes(60); //dateFimPagamento - dataCorrente;
+
+
+            // string mensagem = "Vencimento em " + GetIntervaloDeTempoLegivel(diferenca);
+
+            string mensagem = "Vencimento em " + 
+                                TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+
+            Console.WriteLine("Data Pagamento: " + dateFimPagamento);
+            Console.WriteLine("Data Atual:     " + dataCorrente);
+            Console.WriteLine(mensagem);
+            
+
+            Console.ReadLine();
+        }
+
+        
+        #region static string GetIntervaloDeTempoLegivel(TimeSpan timeSpan)
+        /*
+        static string GetIntervaloDeTempoLegivel(TimeSpan timeSpan)
+        {
+
+            if(timeSpan.Days > 30)
+            {
+                int quantidadeMeses = timeSpan.Days / 30;
+
+                if (quantidadeMeses == 1)
+                {
+                    return "1 mês";
+                }
+                else
+                {
+                    return quantidadeMeses + " meses";
+                }
+            }
+            else if(timeSpan.Days > 7)
+            {
+                int quantidadeSemanas = timeSpan.Days / 7;
+                
+            }
+            return timeSpan.Days + " dias";
+        }
+        */
+        #endregion
+
+    }
+}
