903059c2af58f1819f044b6d11a53e6f33500250
Sara Gabriely SILVAsS <sara.silva10@fatec.sp.gov.sbr>
2019-02-18 13:05:10 -0300

    18.02.19 - 1

--- 01 - Desenvolvedor.NET/Módulo_06 - Strings, expressões regulares e Classe Object/ByteBank/SistemaAgencia/Program.cs
+++ 01 - Desenvolvedor.NET/Módulo_06 - Strings, expressões regulares e Classe Object/ByteBank/SistemaAgencia/Program.cs
@@ -19,6 +19,40 @@ namespace SistemaAgencia
             // pagina?argumentos
             // 012345678
 
+            #region Teste - IsNullOrEmpty
+            // string textoVazio = "";
+            // Console.WriteLine(string.IsNullOrEmpty(textoVazio));
+
+            // string - palavra reservada da linguagem
+            // String (classe) ou string (palavra reservada) - é a mesma coisa
+            #endregion
+
+            string palavra = "moedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino";
+
+            int indice = palavra.IndexOf(nomeArgumento);
+
+            Console.WriteLine(indice);
+            
+            // Resposta: 12
+            Console.WriteLine("Tamanho da string (nomeArgumento): " + nomeArgumento.Length);
+
+            // Resposta: moedaOrigem=real&moedaDestino=dolar
+            Console.WriteLine(palavra);
+
+            // Resposta: moedaDestino=dolar
+            Console.WriteLine(palavra.Substring(indice));
+
+            // Resposta: dolar
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length + 1));
+
+            Console.ReadLine();
+
+            // --------------------------
+
+            ExtratorValorDeArgumentosURL extrator = 
+                    new ExtratorValorDeArgumentosURL("teste");
+
             string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
 
             int indiceInterrogacao = url.IndexOf("?"); // Descobre o indice do "?"
@@ -31,10 +65,7 @@ namespace SistemaAgencia
 
             Console.WriteLine(argumentos); // Depois
 
-
-
-
-
+            
 
             /*
              string temporaria = url + "sufixo";
@@ -42,10 +73,12 @@ namespace SistemaAgencia
             url = temporaria;
 
             ou url += "Sufixo";
-             
              */
 
             Console.ReadLine();
         }
+
+
+
     }
 }
