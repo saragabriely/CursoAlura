a47243c11417c377ec62ef2d502e5c0fea87245c
Sara Gabriely SILVAsS <sara.silva10@fatec.sp.gov.sbr>
2019-02-20 17:49:36 -0300

    20.02.19 - 8º Módulo

---
+++ 01 - Desenvolvedor.NET/Módulo_08 - List T, lambda, linq/ByteBank/Modelos/SaldoInsuficienteException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    // : Exception
+    
+    public class SaldoInsuficienteException : OperacaoFinanceiraException
+    {
+        public double Saldo      { get; }
+        public double ValorSaque { get; }
+
+        public SaldoInsuficienteException() { }
+        
+        public SaldoInsuficienteException(double saldo, double valorSaque)
+            : base("Tentativa de saque do valor de R$ " + valorSaque +
+                   ", em uma conta com saldo de R$ " + saldo)
+        {
+            Saldo       = saldo;
+            ValorSaque  = valorSaque;
+        }
+
+        public SaldoInsuficienteException(string mensagem) : base(mensagem) { }
+
+        // É uma boa pratica colocar um construtor com uma InnerException
+        public SaldoInsuficienteException (string mensagem, Exception excecaoInterna)
+            : base(mensagem, excecaoInterna)
+        {
+
+        }
+
+    }
+}
---
+++ 01 - Desenvolvedor.NET/Módulo_08 - List T, lambda, linq/ByteBank/SistemaAgencia/Estagiario.cs
@@ -0,0 +1,30 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAgencia
+{
+    public class Estagiario : Funcionario
+    {
+        public Estagiario(double salario, string cpf) : base(salario, cpf)
+        {
+
+        }
+
+        public override void AumentarSalario()
+        {
+            // Qualquer código
+        }
+
+        // O INTERNAL faz sentido apenas na classe base - pois é sobre projetos
+
+        protected override double GetBonificacao()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
