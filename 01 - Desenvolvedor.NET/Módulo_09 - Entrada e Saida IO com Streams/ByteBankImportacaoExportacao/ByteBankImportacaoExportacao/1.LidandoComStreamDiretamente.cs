4ea001bb00b84877a3f7eb0bffcd8080663a7cdb
Sara Gabriely SILVAsS <sara.silva10@fatec.sp.gov.sbr>
2019-02-23 15:08:50 -0300

    23.02.19 - 2 - Mód. 09

---
+++ 01 - Desenvolvedor.NET/Módulo_09 - Entrada e Saida IO com Streams/ByteBankImportacaoExportacao/ByteBankImportacaoExportacao/1.LidandoComStreamDiretamente.cs
@@ -0,0 +1,16 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO; // IO = Input e Output
+
+namespace ByteBankImportacaoExportacao
+{
+    class Program
+    {
+
+    }
+}
--- 01 - Desenvolvedor.NET/Módulo_09 - Entrada e Saida IO com Streams/ByteBankImportacaoExportacao/ByteBankImportacaoExportacao/Properties/AssemblyInfo.cs
+++ 01 - Desenvolvedor.NET/Módulo_09 - Entrada e Saida IO com Streams/ByteBankImportacaoExportacao/ByteBankImportacaoExportacao/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associada a um assembly.
-[assembly: AssemblyTitle("Modelos")]
+[assembly: AssemblyTitle("ByteBankImportacaoExportacao")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Modelos")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("ByteBankImportacaoExportacao")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("84d0a5c9-7777-44c9-9ff9-b0514c2d2236")]
+[assembly: Guid("e5923d65-d667-4ddc-978a-870c9d44e580")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Revisão
 //
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
-// usando o '*' como mostrado abaixo:
+// utilizando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
