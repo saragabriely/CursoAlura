4e2c322f2671efcfc8891052a5bae296d3e27aaa
Sara Gabriely SILVAsS <sara.silva10@fatec.sp.gov.sbr>
2019-02-25 15:32:41 -0300

    25.02.19

---
+++ 01_Desenvolvedor.NET/Mód_10_EntityF/Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    //  :IDisposable
+    public class LojaContext : DbContext 
+    {
+        // Essa classe deve permitir a utilização da API do Entity, e para isso 
+        // a classe deve herdar de DbContext
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        // Método de configuração do LojaContext
+        protected override void OnConfiguring(DbContextOptionBuilder optionBuilder)
+        {
+            
+        }
+    }
+}
---
+++ 01_Desenvolvedor.NET/Mód_10_EntityF/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        // Criando o Banco de Dados através do Visual Studio:
+        // Exibir -> Pesquisador de Objetos do SQL Server 
+        //            ou SQl Server Object Explorer
+        // Selecionar (ao lado) Sql Server -> Databases -> Add new Database
+
+        // Criar tabela
+        // Tables -> Botão direito -> Adicionar nova tabela
+        // Colar o script da tabela -> Selecionar 'UPDATE' (canto superior esquerdo)
+        // E depois 'Update Database'
+
+        // Como a aplicação está sendo usada sem o Entity
+        // Ela utiliza o padrão de arquitetura Data Acess Object.
+
+
+        static void Main(string[] args)
+        {
+            //GravarUsandoAdoNet();
+
+            GravarUsandoEntity();
+        }
+
+        private static void GravarUsandoEntity()
+        {
+            // Entity - Criar uma classe que permitira a persistencia de todas as demais
+            // classes.
+
+            Produto p   = new Produto();
+            p.Nome      = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco     = 19.89;
+
+            using (var contexto = new LojaContext())
+            {
+                repo.Adicionar(p);
+            }
+        }
+
+        private static void GravarUsandoAdoNet()
+        {
+            // O objetivo do ADO.NET era criar uma classe que representasse o 
+            // DataAcessObject da classe que seria persistida.
+
+            Produto p   = new Produto();
+            p.Nome      = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco     = 19.89;
+
+            using (var repo = new ProdutoDAO())
+            {
+                repo.Adicionar(p);
+            }
+        }
+    }
+}
