29f9f19cb03ce278ba1b1ff86e75028b23fcf16b
Sara Gabriely SILVAsS <sara.silva10@fatec.sp.gov.sbr>
2019-06-24 11:45:14 -0300

    23.06.19

--- 01_Desenvolvedor.NET/Mód_13_ASP .NET Core Parte 2/ASPNETCore20/Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
+++ 01_Desenvolvedor.NET/Mód_13_ASP .NET Core Parte 2/ASPNETCore20/Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -1,4 +1,5 @@
-﻿using CasaDoCodigo.Repositories;
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,10 +12,13 @@ namespace CasaDoCodigo.Controllers
     {
         // esse campo receberá valor quando for feita a ingeção de dependencia
         private readonly IProdutoRepository produtoRepository;
+        private readonly IPedidoRepository  pedidoRepository;
 
-        public PedidoController(IProdutoRepository produtoRepository)
+        public PedidoController(IProdutoRepository produtoRepository,
+                                IPedidoRepository  pedidoRepository)
         {
             this.produtoRepository = produtoRepository;
+            this.pedidoRepository  = pedidoRepository;
         }
 
         public IActionResult Carrossel()
@@ -22,9 +26,16 @@ namespace CasaDoCodigo.Controllers
             return View(produtoRepository.GetProdutos());
         }
 
-        public IActionResult Carrinho()
+        public IActionResult Carrinho(string codigo)
         {
-            return View();
+            if (!string.IsNullOrEmpty(codigo))
+            {
+                pedidoRepository.AddItem(codigo);
+            }
+
+            Pedido pedido = pedidoRepository.GetPedido(); 
+
+            return View(pedido.Itens);
         }
 
         public IActionResult Cadastro()
@@ -34,7 +45,9 @@ namespace CasaDoCodigo.Controllers
 
         public IActionResult Resumo()
         {
-            return View();
+            Pedido pedido = pedidoRepository.GetPedido();
+
+            return View(pedido);
         }
     }
 }
---
+++ 01_Desenvolvedor.NET/Mód_14_ASP .NET Core Parte 2 + Segurança/ASPNETCore20/Aulas/Aula1/CasaDoCodigo/Repositories/ProdutoRepository.cs
@@ -0,0 +1,45 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class ProdutoRepository : BaseRepository<Produto>, IProdutoRepository
+    {
+        // novo construtor
+        public ProdutoRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+
+        public IList<Produto> GetProdutos()
+        {
+           return dbSet.ToList();
+        }
+
+        public void SaveProdutos(List<Livro> livros)
+        {
+            foreach (var livro in livros)
+            {
+                // o IF evita de mostrar livros repetidos
+                if (!dbSet.Where(l => l.Codigo == livro.Codigo).Any())
+                {
+                    // Any() - traz verdadeiro caso encontre o livro
+                    // carrega as informaçõs em memória
+                    dbSet.Add(new Produto(livro.Codigo, livro.Nome, livro.Preco));
+                }
+            }
+            contexto.SaveChanges(); // salva as informações no banco
+        }
+    }
+
+    public class Livro
+    {
+        public string  Codigo { get; set; }
+        public string  Nome   { get; set; }
+        public decimal Preco  { get; set; }
+        
+    }
+}
